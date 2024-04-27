da904cbd3bbd30583a7f34276e626d78092b3039
Richard <richard.skoda72@gmail.com>
2024-04-27 09:17:00 +0200

    nuxt basic course

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav>
+    <NuxtLink to="/">Home</NuxtLink>
+  </nav>
+  <main>
+    <slot />
+  </main>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
